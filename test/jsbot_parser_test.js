4024fa3a25a90f2c9878282da5f9fe5fa6e4f208
Daniel Ravina <daniel@intergalactic.com>
2015-10-21 16:14:23 -0700

    class relationship

--- test/jsbot_parser_test.js
+++ test/jsbot_parser_test.js
@@ -4,17 +4,17 @@ import Chai  from 'chai';
 var expect = Chai.expect;
 
 describe("JSBot Parser", () =>  {
-  let jsbot; // define bot
+  let parser; // define bot
 
   before(() => {
-    jsbot = JSBotParser(); // initialize Bot
+    parser = JSBotParser(); // initialize Bot
   })
 
   it("reports a missing semicolon", () => {
     let jsString = 'var a = 5';
 
-    jsbot.process(jsString)
-    let error = jsbot.getIssues().errors[0];
+    parser.process(jsString)
+    let error = parser.getPayload().errors[0];
 
     expect(error.reason).to.contain("Missing semicolon");
     expect(error.line).to.equal(1);
@@ -23,8 +23,8 @@ describe("JSBot Parser", () =>  {
 
   it("reports a missing bracket", () => {
     let jsString = 'function goo(){';
-    jsbot.process(jsString)
-    let error = jsbot.getIssues().errors[0];
+    parser.process(jsString)
+    let error = parser.getPayload().errors[0];
     expect(error.reason).to.contain("Unmatched");
     expect(error.line).to.equal(1);
 
@@ -33,8 +33,8 @@ describe("JSBot Parser", () =>  {
 
   it("reports unused variables", () => {
     let jsString = 'function goo(){}';
-    jsbot.process(jsString)
-    let unused = jsbot.getIssues().unused[0];
+    parser.process(jsString)
+    let unused = parser.getPayload().unused[0];
 
     expect(unused.name).to.contain("goo");
     expect(unused.line).to.equal(1);
@@ -43,20 +43,20 @@ describe("JSBot Parser", () =>  {
 
   it("reports multiple issues", () => {
     let jsString = "function goo() { foo = 3\n p.start() }";
-    jsbot.process(jsString);
+    parser.process(jsString);
 
     // errors:
     //[ { line: 1, character: 24, reason: 'Missing semicolon.' },
     //{ line: 2, character: 10, reason: 'Missing semicolon.' },
     //{ line: 1, character: 17, reason: '\'foo\' is not defined.' },
     //{ line: 2, character: 1, reason: '\'p\' is not defined.' } ]
-    var errors = jsbot.getIssues().errors
+    var errors = parser.getPayload().errors
     expect(errors).to.have.length(4);
     // can keep testing here but the order is not reliable
 
     // unused:
     // [ { name: 'goo', line: 1, character: 10 } ]
-    var unused = jsbot.getIssues().unused
+    var unused = parser.getPayload().unused
     expect(unused).to.have.length(1);
   })
 
--- test/jsbot_presenter_test.js
+++ test/jsbot_presenter_test.js
@@ -1,8 +1,24 @@
-import JSBot from "lib/JSBotParser";
+import JSBotParser from "lib/JSBotParser";
+import JSBotPresenter from "lib/JSBotPresenter";
 import Chai  from 'chai';
 
 var expect = Chai.expect;
 
 describe("JSBot Presenter", () =>  {
+  let parser, presenter;
 
+  before(() => {
+    parser    = JSBotParser();
+    presenter = JSBotPresenter();
+  })
+
+  it("presents the error in a slackable way", () => {
+    let jsString = 'var a = 5';
+
+    parser.process(jsString)
+    presenter.process(parser.getPayload())
+
+    expect
+
+  })
 })
